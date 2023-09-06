a8fec46b22dc38996a923b71df7c74fdccfd1f3c
Martin Denis <martin.2nis@gmail.com>
2023-09-06 22:20:28 +0200

    Added favicon, logo and manifest.json

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,16 +3,21 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Wishlists with Laravel & Vue</title>
+        <title>Wishr</title>
 
         @vite('resources/sass/app.scss')
         @vite('resources/js/app.js')
+
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.icon') }}" sizes="32x32">
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+        <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}"><!-- 180×180 -->
+        <link rel="manifest" href="{{ asset('manifest.webmanifest.json') }}">
+
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet" media="screen">
     </head>
-    <body id="app" class="bg-slate-950 text-slate-400 subpixel-antialiased overflow-y-scroll flex font-medium
-     max-lg:pb-20 lg:max-2xl:pl-60">
+    <body id="app" class="bg-slate-950 text-slate-400 subpixel-antialiased overflow-y-scroll flex items-stretch font-medium">
     </body>
 
     <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./resources/views/app.blade.php", "./resources/**/*.{vue,html,js}"],
   theme: {
