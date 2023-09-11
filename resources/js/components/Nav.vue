2c59fcd8612255778b5a71be4864069a5cb6d2c3
Martin Denis <martin.2nis@gmail.com>
2023-09-11 19:32:34 +0200

    Improved navigation style

--- resources/js/components/Nav.vue
+++ resources/js/components/Nav.vue
@@ -1,43 +1,17 @@
 <script setup>
-  import { RouterLink } from 'vue-router'
   import { useStore } from 'vuex'
+  import NavItem from './NavItem.vue'
 
   const store = useStore()
 </script>
 
 <template>
-  <nav class="bg-slate-900 border-slate-800 max-lg:border-t lg:border-r lg:py-6 fixed z-50 w-full bottom-0 left-0 h-[4.5rem] lg:h-full lg:w-60">
-    <ul class="px-2 md:px-4 max-w-sm mx-auto flex lg:gap-2 lg:flex-col max-lg:text-xs h-full justify-center">
-      <li class="hidden lg:flex my-3">
-        <RouterLink :to="{name: 'Home'}" class="nav-logo block active:opacity-75 duration-300 px-4 py-2">
-          <img src="../../img/wishr.svg" alt="Wishr" width="40">
-        </RouterLink>
-      </li>
-      <li class="max-lg:flex-1 min-w-0">
-        <RouterLink :to="{name: 'Home'}" class="nav-link duration-300 flex flex-col gap-1 lg:flex-row lg:gap-4 h-full items-center justify-center lg:justify-start max-lg:text-center px-2 md:px-4 py-2 font-semibold text-slate-400 active:bg-slate-800 lg:rounded-md" active-class="menu-active">
-          <svg class="max-lg:text-lg"><use href="#home"/></svg>
-          Home
-        </RouterLink>
-      </li>
-      <li class="max-lg:flex-1 min-w-0">
-        <RouterLink :to="{name: 'Discover'}" class="nav-link duration-300 flex flex-col gap-1 lg:flex-row lg:gap-4 h-full items-center justify-center lg:justify-start max-lg:text-center px-2 md:px-4 py-2 font-semibold text-slate-400 active:bg-slate-800 lg:rounded-md" active-class="menu-active">
-          <svg class="max-lg:text-lg"><use href="#compass"/></svg>
-          Discover
-        </RouterLink>
-      </li>
-      <li class="max-lg:flex-1 min-w-0">
-        <RouterLink :to="{name: 'Friends'}" class="nav-link duration-300 flex flex-col gap-1 lg:flex-row lg:gap-4 h-full items-center justify-center lg:justify-start max-lg:text-center px-2 md:px-4 py-2 font-semibold text-slate-400 active:bg-slate-800 lg:rounded-md" active-class="menu-active">
-          <svg class="max-lg:text-lg"><use href="#users"/></svg>
-          Friends
-        </RouterLink>
-      </li>
-      <li class="hidden mt-auto lg:block"><hr class="border-slate-800"></li>
-      <li class="max-lg:flex-1 min-w-0 lg:mt-2">
-        <RouterLink :to="{name: 'Account'}" class="nav-link duration-300 flex flex-col gap-1 lg:flex-row lg:gap-4 h-full items-center justify-center lg:justify-start max-lg:text-center px-2 md:px-4 py-2 font-semibold text-slate-400 active:bg-slate-800 lg:rounded-md" active-class="menu-active">
-          <svg class="max-lg:text-lg shrink-0"><use href="#setting"/></svg>
-          <span class="block w-full leading-tight text-ellipsis overflow-hidden whitespace-nowrap">{{ store.getters.user.name }}</span>
-        </RouterLink>
-      </li>
+  <nav class="bg-slate-900 lg:py-6 fixed z-50 w-full bottom-0 left-0 h-16 lg:h-full lg:w-24">
+    <ul class="px-2 max-w-sm mx-auto flex lg:gap-3 lg:flex-col h-full justify-center">
+      <NavItem :to="{name: 'Home'}" icon="wishr" text="Home"></NavItem>
+      <NavItem :to="{name: 'Discover'}" icon="compass" text="Discover"></NavItem>
+      <NavItem :to="{name: 'Friends'}" icon="users" text="Friends"></NavItem>
+      <NavItem :to="{name: 'Account'}" icon="setting" text="Account" class="lg:mt-auto"></NavItem>
     </ul>
   </nav>
 </template>
@@ -53,7 +27,11 @@
     }
 
     .nav-link {
-      @apply hover:text-slate-300 hover:bg-slate-800;
+      @apply hover:text-slate-300;
+    }
+
+    .nav-link:hover .nav-link-icon {
+      @apply bg-slate-800;
     }
   }
 </style>
---
+++ resources/js/components/NavItem.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { RouterLink } from 'vue-router'
+
+  defineProps(['to', 'icon', 'text'])
+</script>
+
+<template>
+  <li class="max-lg:flex-1 min-w-0">
+    <RouterLink :to="to" class="group nav-link duration-300 block h-full items-center justify-center lg:justify-start text-center pt-0.5 pb-2 font-semibold text-xs text-slate-400lg:rounded-md" active-class="menu-active">
+      <span class="nav-link-icon rounded-full w-10 h-10 lg:w-11 lg:h-11 -mb-2 lg:-mb-0.5 mx-auto flex justify-center items-center duration-300 group-active:bg-slate-800">
+        <svg class="text-lg"><use :href="'#' + icon" /></svg>
+      </span>
+      {{ text }}
+    </RouterLink>
+  </li>
+</template>
