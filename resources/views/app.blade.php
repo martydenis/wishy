8d0decba6417d49ac08de8c962243e0d47beccde
Martin Denis <martin.2nis@gmail.com>
2023-09-13 17:43:32 +0200

    Yep, changed name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Wishr</title>
+        <title>Wishy</title>
 
         @vite('resources/sass/app.scss')
         @vite('resources/js/app.js')
